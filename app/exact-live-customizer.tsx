26374b2d815722d782f3d8ae1e570d4d92ddc3e7
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 06:53:19 +0200

    Mount touch rescue for universal 3D zoom

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -1,5 +1,6 @@
 import AtelierBagRendererV7 from "./atelier-bag-renderer-v7";
 import BagBuilderCanvas3D from "./bag-builder-canvas3d";
+import BagBuilderCanvas3DTouchRescue from "./bag-builder-canvas3d-touch-rescue";
 import BagBuilderEngine from "./bag-builder-engine";
 import BagBuilderPro3D from "./bag-builder-pro3d";
 
@@ -9,5 +10,6 @@ export default function ExactLiveCustomizer() {
     <AtelierBagRendererV7 />
     <BagBuilderPro3D />
     <BagBuilderCanvas3D />
+    <BagBuilderCanvas3DTouchRescue />
   </>;
 }
