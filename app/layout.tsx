ae62dd9b91b63e4be033a4b198a518e0a8ce4cd0
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 19:39:52 +0200

    Load handbag construction calibration styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ import "./bag-builder-3d-enhancer.css";
 import "./bag-builder-pro3d.css";
 import "./bag-builder-canvas3d.css";
 import "./bag-builder-material-pass.css";
+import "./bag-builder-construction-pass.css";
 import "./bag-builder-commerce.css";
 import "./bag-builder-admin.css";
 import "./customizer-admin.css";
