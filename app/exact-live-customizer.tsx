59e1ace4dd3594d3c30532e74cc04870ca8fa613
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 06:15:16 +0200

    Wire deep zoom controller into Pro 3D builder

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -1,11 +1,13 @@
 import AtelierBagRendererV7 from "./atelier-bag-renderer-v7";
 import BagBuilderEngine from "./bag-builder-engine";
 import BagBuilderPro3D from "./bag-builder-pro3d";
+import BagBuilderPro3DController from "./bag-builder-pro3d-controller";
 
 export default function ExactLiveCustomizer() {
   return <>
     <BagBuilderEngine />
     <AtelierBagRendererV7 />
     <BagBuilderPro3D />
+    <BagBuilderPro3DController />
   </>;
 }
