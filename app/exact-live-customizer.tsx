ecfbaf9e964a8184408cf2567aecd59b56a057c8
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 03:59:23 +0200

    Make Pro 3D the primary Bag Builder preview

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -1,13 +1,11 @@
 import AtelierBagRendererV7 from "./atelier-bag-renderer-v7";
-import BagBuilder3DEnhancer from "./bag-builder-3d-enhancer";
 import BagBuilderEngine from "./bag-builder-engine";
-import BagBuilderReal3D from "./bag-builder-real3d";
+import BagBuilderPro3D from "./bag-builder-pro3d";
 
 export default function ExactLiveCustomizer() {
   return <>
     <BagBuilderEngine />
     <AtelierBagRendererV7 />
-    <BagBuilderReal3D />
-    <BagBuilder3DEnhancer />
+    <BagBuilderPro3D />
   </>;
 }
