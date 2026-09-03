75cbbd181676c0ea9a8257635fe238f30f16a89b
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 16:03:22 +0200

    Load approved visual customizer layout overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ import "./bag-builder-canvas3d.css";
 import "./bag-builder-commerce.css";
 import "./bag-builder-admin.css";
 import "./customizer-admin.css";
+import "./bag-builder-reference-ui.css";
 
 const display = Cormorant_Garamond({
   variable: "--font-display",
