c5b93afa9a5a15ac71cb5489ff8d79328f49c160
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 19:50:08 +0200

    Promote variable-depth fidelity renderer to primary 3D view

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -1,10 +1,10 @@
 import AtelierBagRendererV7 from "./atelier-bag-renderer-v7";
-import BagBuilderAtelier3D from "./bag-builder-atelier3d";
 import BagBuilderCanvas3D from "./bag-builder-canvas3d";
 import BagBuilderCanvas3DTouchRescue from "./bag-builder-canvas3d-touch-rescue";
 import BagBuilderCommerce from "./bag-builder-commerce";
 import BagBuilderConstructionPass from "./bag-builder-construction-pass";
 import BagBuilderEngine from "./bag-builder-engine";
+import BagBuilderFidelity3D from "./bag-builder-fidelity3d";
 import BagBuilderMaterialPass from "./bag-builder-material-pass";
 import BagBuilderPro3DTouchRescue from "./bag-builder-pro3d-touch-rescue";
 import BagBuilderReferenceExperience from "./bag-builder-reference-experience";
@@ -14,7 +14,7 @@ export default function ExactLiveCustomizer() {
   return <>
     <BagBuilderEngine />
     <AtelierBagRendererV7 />
-    <BagBuilderAtelier3D />
+    <BagBuilderFidelity3D />
     <BagBuilderPro3DTouchRescue />
     <BagBuilderCanvas3D />
     <BagBuilderCanvas3DTouchRescue />
