9c2dc4c2bbc6d1001c578c68eb35bc99d148ab88
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-03 03:23:49 +0200

    Activate interactive 3D handbag preview

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -1,9 +1,11 @@
 import AtelierBagRendererV7 from "./atelier-bag-renderer-v7";
 import BagBuilderEngine from "./bag-builder-engine";
+import BagBuilderWebGL3D from "./bag-builder-webgl3d";
 
 export default function ExactLiveCustomizer() {
   return <>
     <BagBuilderEngine />
     <AtelierBagRendererV7 />
+    <BagBuilderWebGL3D />
   </>;
 }
