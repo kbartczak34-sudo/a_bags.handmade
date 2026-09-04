38cefb34274edac908d360473c7f30f548c7508b
kbartczak34-sudo <kbartczak34@gmail.com>
2026-09-04 14:58:06 +0200

    Mount final WebGL 3D v2 in customer customizer

--- app/exact-live-customizer.tsx
+++ app/exact-live-customizer.tsx
@@ -9,7 +9,7 @@ import BagBuilderAutosave from "./bag-builder-autosave";
 import BagBuilderCheckoutHandoff from "./bag-builder-checkout-handoff";
 import BagBuilderCommerce from "./bag-builder-commerce";
 import BagBuilderEngine from "./bag-builder-engine";
-import BagBuilderFidelity3D from "./bag-builder-fidelity3d";
+import BagBuilderFinalWebGL3D from "./bag-builder-final-webgl3d";
 import BagBuilderFinal3DController from "./bag-builder-final3d-controller";
 import BagBuilderMaterialInfo from "./bag-builder-material-info";
 import BagBuilderPhotoTrue from "./bag-builder-photo-true";
@@ -31,7 +31,7 @@ import BagBuilderViewSync from "./bag-builder-view-sync";
 export default function ExactLiveCustomizer() {
   return <>
     <BagBuilderEngine />
-    <BagBuilderFidelity3D />
+    <BagBuilderFinalWebGL3D />
     <BagBuilderFinal3DController />
     <BagBuilderPro3DTouchRescue />
     <BagBuilderRendererFallback />
